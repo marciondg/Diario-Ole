50b68ff0097b357c5a356370d327de4063a7b587
Marcio Garozzo <mgarozzo@est.frba.utn.edu.ar>
2021-01-26 17:18:57 -0300

    saco funcion de .js

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -132,24 +132,3 @@ function parsearInfo(respuesta){
     episodes =  episode.results;
     return episodes;
 } */
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
